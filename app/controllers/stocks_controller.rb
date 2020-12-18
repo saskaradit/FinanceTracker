c8f6df3650bb0195778035cec441762552ae81ca
Rad Saskara <radsaskara@Rads-MacBook-Pro.local>
2020-12-18 13:56:30 +0700

    displaying stocks in the browser

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,24 @@
 class StocksController < ApplicationController
 
   def search
-    stock = Stock.new_lookup(params[:stock])
-    render json: stock
+
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        respond_to do |format|
+          format.js {render partial: 'users/result'}
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "You should enter a valid symbol"
+          format.js {render partial: 'users/result'}
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "You should enter a symbol"
+        format.js {render partial: 'users/result'}
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
   get 'my_portfolio', to: 'users#my_portfolio'
-  get 'search_stock', to: 'stock#search'
+  get 'search_stock', to: 'stocks#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
