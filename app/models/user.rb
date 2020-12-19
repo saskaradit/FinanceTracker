d0b0ec73f9776de74bef74e472928e70d8b8a894
Rad Saskara <radsaskara@Rads-MacBook-Pro.local>
2020-12-19 21:43:41 +0700

    adding friends table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -18,6 +20,10 @@ class User < ApplicationRecord
 
   def can_track_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_tracked?(ticker_symbol)
-    
+  end
+
+  def full_name
+    return "#{first_name} #{last_name}" if first_name || last_name
+    "User"
   end
 end
