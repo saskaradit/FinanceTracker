a154ae48e2b8e2d58ade0a4f9d115c28167e522e
Rad Saskara <saskaraxyz@gmail.com>
2020-12-20 16:40:47 +0700

    finiship up the production

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,19 @@
+class FriendshipsController < ApplicationController
+  def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "Started Following #{friend.full_name}"
+    else
+      flash[:alert] = "There is something wrong"
+    end
+    redirect_to friends_path
+  end
+
+  def destroy
+    friendship = current_user.friendships.where(friend_id: params[:id]).first
+    friendship.destroy
+    flash[:notice] = "Stopped following"
+    redirect_to friends_path
+  end
+end
